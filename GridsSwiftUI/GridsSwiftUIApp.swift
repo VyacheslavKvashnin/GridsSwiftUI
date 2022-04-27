df987933cd4c4da8038784a60607fdc291cbc85a
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-27 19:26:29 +0500

    Initial Commit

---
+++ GridsSwiftUI/GridsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsSwiftUIApp.swift
+//  GridsSwiftUI
+//
+//  Created by Вячеслав Квашнин on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct GridsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
